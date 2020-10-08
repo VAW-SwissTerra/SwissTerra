c7349140dd2b6dbff725cdb5f65825c6c948795b
Erik Holmlund <erik-holmlund@hotmail.com>
2020-10-08 12:57:29 +0200

    Added ASIFT function and image-matching candidates extraction

--- terra/metadata/image_meta.py
+++ terra/metadata/image_meta.py
@@ -74,3 +74,120 @@ def read_metadata() -> pd.DataFrame:
     metadata = pd.read_pickle(CACHE_FILES["image_meta"])
 
     return metadata
+
+
+def get_matching_candidates(angle_threshold: float = 45.0, distance_threshold: float = 1500):
+    """
+    Find images that are relatively close and look the same direction, but are not in the same base (stereo-pair).
+
+    This may be the ugliest code I've ever written! But it works, and it seems to be efficient.
+
+    param: angle_threshold: The maximum allowed angle difference in degrees.
+    param: distance_threshold: The maximum allowed distance between the two images in metres.
+
+    return: pairs: Which image pairs may get matches between them.
+    """
+
+    image_meta = read_metadata()
+
+    # Generate the vectorized functions to filter pairs from
+    @np.vectorize
+    def angle_diff(angle_a: float, angle_b: float) -> float:
+        """
+        Calculate an unsigned angle difference.
+
+        param: angle_a: The first angle
+        param: angle_b: The second angle
+
+        return: diff: The unsigned angle difference
+        """
+        diff = angle_a - angle_b
+        return abs((diff + 180) % 360 - 180)
+
+    @np.vectorize
+    def get_distance(xyz_a: str, xyz_b: str) -> float:
+        """
+        Get the distance between two position strings ("easting,northing,altitude").
+
+        param: xyz_a: The first position string.
+        param: xyz_b: The second position string.
+
+        return: diff: The distance in meters.
+        """
+        position_a = np.array(xyz_a.split(",")).astype(float)
+        position_b = np.array(xyz_b.split(",")).astype(float)
+        diff = np.linalg.norm(position_a - position_b)
+        return diff
+
+    @np.vectorize
+    def has_different_base(base_a: int, base_b: int) -> bool:
+        """
+        Check whether two base (stereo-pair) numbers are the different.
+
+        param: station_a: The first base
+        param: station_b: The second base 
+
+        return: has_different_station: Whether the two bases are different.
+
+        """
+        return base_a != base_b
+
+    #
+    # Check the angle criterion
+    #
+    # Create a meshgrid of yaw (viewing direction) angles
+    xx_grid, yy_grid = np.meshgrid(image_meta["yaw"], image_meta["yaw"])
+    # Get the indices of the pairs that fill the angle threshold
+    fulfilling_angle = np.unique(np.sort(np.argwhere(angle_diff(xx_grid, yy_grid) < angle_threshold), axis=1), axis=0)
+
+    #
+    # Check the distance criterion
+    #
+    # Extract the values (as a 2D array)
+    xyzs = image_meta[["easting", "northing", "altitude"]].values
+    # Convert each image's coordinates into a "easting,northing,altitude" string
+    # This is because I can't find out how to get the vectorized function to take an array!
+    xyz_strings = np.apply_along_axis(lambda x: ",".join(x.astype(str)), axis=1, arr=xyzs)
+    # Create a meshgrid of position strings
+    xx_grid, yy_grid = np.meshgrid(xyz_strings, xyz_strings)
+    # Get the indices of the pairs that fill the distance threshold
+    fulfilling_distance = np.unique(
+        np.sort(np.argwhere(get_distance(xx_grid, yy_grid) < distance_threshold), axis=1), axis=0)
+
+    #
+    # Check that the images are not in the same base (stereo-pair)
+    #
+    # Create a meshgrid of base numbers
+    xx_grid, yy_grid = np.meshgrid(image_meta["Base number"], image_meta["Base number"])
+    # Get the indices of the pairs that fill the not-same-base threshold
+    fulfilling_base = np.unique(np.sort(np.argwhere(has_different_base(xx_grid, yy_grid)), axis=1), axis=0)
+
+    #
+    # Merge the criterion results
+    #
+    # Merge angles and distances
+    fulfilling_angle_distance = fulfilling_angle[(fulfilling_angle[:, None] == fulfilling_distance).all(-1).any(-1)]
+    # Merge angles+distances with base numbers
+    fulfilling_all = fulfilling_angle_distance[(
+        fulfilling_angle_distance[:, None] == fulfilling_base).all(-1).any(-1)]
+
+    @np.vectorize
+    def get_filename(index: int) -> str:
+        """
+        Get the corresponding filename of a positional index.
+
+        param: index: The locational index in the image_meta dataframe.
+
+        return: filename: The filename of the image.
+        """
+        return image_meta.iloc[index].loc["Image file"]
+
+    # Extract the filenames of the pairs
+    pairs = get_filename(fulfilling_all)
+
+    print(pairs.shape)
+    return pairs
+
+
+if __name__ == "__main__":
+    get_matching_candidates()
--- terra/processing/processing.py
+++ terra/processing/processing.py
@@ -3,10 +3,17 @@ import os
 import subprocess
 from typing import Optional
 
+import cv2
+import numpy as np
+import pandas as pd
 import statictypes
 
 from terra import constants, files
 from terra.processing import inputs
+
+CACHE_FILES = {
+    "asift_temp_dir": os.path.join(inputs.TEMP_DIRECTORY, "asift_temp")
+}
 
 
 @statictypes.enforce
@@ -74,4 +81,50 @@ def run_pdal_pipeline(pipeline: str, output_metadata_file: Optional[str] = None)
     if output_metadata_file is not None:
         commands += ["--metadata", output_metadata_file]
 
-    subprocess.run(commands, input=pipeline,  check=True, encoding="utf-8")
+    subprocess.run(commands, input=pipeline, check=True, encoding="utf-8")
+
+
+def match_asift(image1: np.ndarray, image2: np.ndarray) -> pd.DataFrame:
+    """
+    Find matches between two images using the ASIFT (Affine-Scale Invariant Feature transform).
+
+    param: image1: The first image to match.
+    param: image2: The second image to match.
+
+    return: matches: The computed matches between the images.
+    """
+    # Trim the image to remove the frame and reduce it so ASIFT can use it
+    side_trim = 500  # To remove the frame
+    new_width = 2500  # To downscale the image so ASIFT can work
+    os.makedirs(CACHE_FILES["asift_temp_dir"], exist_ok=True)
+
+    # Go through each image and crop, resize, then save it as a PNG (needed for ASIFT)
+    for i, image in enumerate([image1, image2]):
+        trimmed_image = image[side_trim:-side_trim, side_trim:-side_trim]
+
+        new_height = int(trimmed_image.shape[0] * (new_width / trimmed_image.shape[1]))
+        resized_image = cv2.resize(trimmed_image, (new_width, new_height))
+        assert resized_image.shape[1] == new_width  # Check that everything went right
+
+        cv2.imwrite(os.path.join(CACHE_FILES["asift_temp_dir"], f"image_{i + 1}.png"), resized_image)
+
+    # Files that ASIFT needs/produces (first/second image input, horizontal/vertical match plot, matches, and keypoints)
+    asift_files = ["image_1.png", "image_2.png", "matches_vert.png",
+                   "matches_horiz.png", "matches.txt", "keys1.txt", "keys2.txt"]
+    # Put the commands in order (asift INPUTS+OUTPUTS 0). The 0 means don't resize to 800x600
+    asift_commands = ["asift"] + [os.path.join(CACHE_FILES["asift_temp_dir"], filename)
+                                  for filename in asift_files] + ["0"]
+    # Run ASIFT
+    subprocess.run(asift_commands, check=True)
+
+    # Load the matches produced by ASIFT
+    matches = pd.read_csv(os.path.join(CACHE_FILES["asift_temp_dir"], "matches.txt"), delimiter="  ",
+                          skiprows=1, engine="python", names=["img1_x", "img1_y", "img2_x", "img2_y"])
+
+    # Convert the match coordinates from trimmed/scaled to the original size
+    for label, image in zip(["img1", "img2"], [image1, image2]):
+        matches[[f"{label}_x", f"{label}_y"]] *= (image.shape[1] - side_trim * 2) / new_width
+    matches += side_trim
+
+    matches.to_csv(os.path.join(CACHE_FILES["asift_temp_dir"], "fixed_matches.csv"))
+    return matches
