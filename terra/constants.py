b0ca10df69c7e800df576d11dbb418ef9b63e227
Erik Holmlund <erik-holmlund@hotmail.com>
2020-10-20 10:50:50 +0200

    Added type-safe constant class with readonly status instead of editable dictionary

--- terra/constants.py
+++ terra/constants.py
@@ -1,10 +1,42 @@
 """Constant parameters that are used throughout the module."""
+from typing import Any
 
-CONSTANTS = {
+import statictypes
 
-    "scanning_resolution": 21e-6,  # The scanning resolution of the images in meters (21 micrometers)
-    "crs_epsg": "EPSG::21781",  # CH1903 / LV03
-    "max_point_cloud_threads": 10,  # How many threads to cap the program on to not run out of memory
-    "dense_cloud_min_confidence": 3,  # The minimum proprietary Metashape confidence value for a dense cloud to accept.
-    "dem_resolution": 5.0,  # The resolution in metres to generate DEMs with.
-}
+
+class Constants:
+    """Readonly document constants."""
+
+    scanning_resolution: float = 21e-6  # The scanning resolution of the images in meters (21 micrometers)
+    crs_epsg: str = "EPSG::21781"  # CH1903 / LV03
+    max_point_cloud_threads: int = 10  # How many threads to cap the program on to not run out of memory
+    dense_cloud_min_confidence: int = 3  # The minimum proprietary Metashape confidence value to accept.
+    dem_resolution: float = 5.0  # The resolution in metres to generate DEMs with.
+
+    @statictypes.enforce
+    def __getitem__(self, key: str) -> Any:
+        """
+        Get an item like a dict.
+
+        param: key: The attribute name.
+
+        return: attribute: The value of the attribute.
+        """
+        attribute = self.__getattribute__(key)
+        return attribute
+
+    @staticmethod
+    def raise_readonly_error(key, value):
+        """Raise a readonly error if a value is trying to be set."""
+        raise ValueError(f"Trying to change a constant. Key: {key}, value: {value}")
+
+    def __setattr__(self, key, value):
+        """Override the Constants.key = value action."""
+        self.raise_readonly_error(key, value)
+
+    def __setitem__(self, key, value):
+        """Override the Constants['key'] = value action."""
+        self.raise_readonly_error(key, value)
+
+
+CONSTANTS = Constants()
